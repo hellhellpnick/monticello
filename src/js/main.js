e192c3077b4bd82ad042cc8cf2dd83ed3a5bbeca
hellpnick <48753830+hellhellpnick@users.noreply.github.com>
2020-06-15 16:29:43 +0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -4,19 +4,38 @@ function initSlider() {
         infinite: true,
         speed: 300,
         slidesToShow: 1,
-        autoplay: true
+        autoplay: true,
+        prevArrow: false,
+        vertical: true,
+        nextArrow: $(".arrow-down"),
+        dotsClass: 'slick-dots',
+    });
+}
+
+function SliderLatest() {
+    $('.news__slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: $(".arrow-left"),
+        nextArrow: $(".arrow-right"),
+        dotsClass: 'card-dots',
     });
 }
 
 $(document).on('ready', function () {
     initSlider();
-
-    $('.news__slider').slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: $(".arrow-left"),
-        nextArrow: $(".arrow-right"),
+    SliderLatest();
+    $(document).delegate('.open', 'click', function (event) {
+        $(this).addClass('oppenned');
+        event.stopPropagation();
+    })
+    $(document).delegate('body', 'click', function (event) {
+        $('.open').removeClass('oppenned');
+    })
+    $(document).delegate('.cls', 'click', function (event) {
+        $('.open').removeClass('oppenned');
+        event.stopPropagation();
     });
 });
